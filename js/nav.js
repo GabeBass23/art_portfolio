d5450bf371fa9f9f44cebcc7f69b06b2936548d0
GabeBass23 <gabebass23@gmail.com>
2024-11-25 16:19:58 -0500

    new page

--- js/nav.js
+++ js/nav.js
@@ -8,10 +8,13 @@ if(document.title === "Grayscale"){
 if(document.title === "Color"){
     index = 2;
 }
+if(document.title === "Commissions"){
+    index = 3;
+}
 
 let thing = document.querySelectorAll("nav > ul > li");
 
-for(i = 0; i < 3; ++i){
+for(i = 0; i < 4; ++i){
     if(i === index){
         thing[i].classList.add("currentTab");
         thing[i].classList.remove("notCurrentTab");
